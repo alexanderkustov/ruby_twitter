0b7d9e36d5d9c4fccd9c57d901a2e45e08475776
Alexander Kustov <knx@MacBook-de-Alexander-Kustov.local>
2012-07-07 17:39:51 +0100

    add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,9 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def show
+def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def create
